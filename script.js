46006d509e214c9a9ce6fb38b4d53bccdba23a8f
HPTR <hugo_reed@hotmail.co.uk>
2020-12-22 10:26:47 +0000

    Create message variable arrays

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const randomNum = num => {
     return Math.floor(Math.random()*num);
 };
+
+const games = ["Minecraft", "Fortnite", "Hades", "Doom", "Tetris", "Cyberpunk 2077", "Skyrim", "Death Stranding", "Dark Souls"];
+const qualities = ["a fantasic", "a great", "a mediocre", "an okay", "a terrible", "an awful", "a boring", "a fun"];
+const recommendations = ["You should buy it now!", "Maybe wait until it's on sale.", "Don't waste your money on it.", "It's worth checking out if you're a fan of other games in the genre.", "Buy it if you've always wanted a game to put you to sleep.", "It might be the best game I've ever played", "It might be the worst game I've ever played"];
+
