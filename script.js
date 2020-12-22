ffa5f972e51bd517392807cd9afa11708f0a95bc
HPTR <hugo_reed@hotmail.co.uk>
2020-12-22 10:34:21 +0000

    Add message creation function

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ const games = ["Minecraft", "Fortnite", "Hades", "Doom", "Tetris", "Cyberpunk 20
 const qualities = ["a fantasic", "a great", "a mediocre", "an okay", "a terrible", "an awful", "a boring", "a fun"];
 const recommendations = ["You should buy it now!", "Maybe wait until it's on sale.", "Don't waste your money on it.", "It's worth checking out if you're a fan of other games in the genre.", "Buy it if you've always wanted a game to put you to sleep.", "It might be the best game I've ever played", "It might be the worst game I've ever played"];
 
+const createMessage = () = => {
+    return `${games[randomNum(games.length)]} is ${qualities[randomNum(qualities.length)]} game. ${recommendations[randomNum(recommendations.length)]} I'd give it a solid ${scores[randomNum(scores.length)]}.`
+};
+
