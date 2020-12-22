504fa7a64e9bcf5d0c0a9f9ad69e310f3e8bf509
HPTR <hugo_reed@hotmail.co.uk>
2020-12-22 11:34:40 +0000

    Change function creation method

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const qualities = ["a fantasic", "a great", "a mediocre", "an okay", "a terrible
 const recommendations = ["You should buy it now!", "Maybe wait until it's on sale.", "Don't waste your money on it.", "It's worth checking out if you're a fan of other games in the genre.", "Buy it if you've always wanted a game to put you to sleep.", "It might be the best game I've ever played.", "It might be the worst game I've ever played."];
 const scores = ["1/10", "2/10", "3/10", "4/10", "5/10", "6/10", "7/10", "8/10", "9/10", "10/10"]
 
-const createMessage = () = => {
-    return `${games[randomNum(games.length)]} is ${qualities[randomNum(qualities.length)]} game. ${recommendations[randomNum(recommendations.length)]} I'd give it a solid ${scores[randomNum(scores.length)]}.`
+function createMessage() {
+    document.getElementById("review").innerHTML = `${games[randomNum(games.length)]} is ${qualities[randomNum(qualities.length)]} game. ${recommendations[randomNum(recommendations.length)]} I'd give it a solid ${scores[randomNum(scores.length)]}.`
 };
-
